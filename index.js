304082ae8e70eb998a4d78782139ffa0eb06dfb2
Henry Liu <liuhenry64@gmail.com>
2022-03-13 22:39:43 -0400

    Add image slider functionality

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
   $("#next-arrow").on("click", function () {
-    console.log("right");
+    var currentImg = $(".active");
+    var nextImg = currentImg.next();
+    if (nextImg.length) {
+      currentImg.removeClass("active").css("z-index", -10);
+      nextImg.addClass("active").css("z-index", 10);
+    }
   });
   $("#prev-arrow").on("click", () => {
-    console.log("left");
+    var currentImg = $(".active");
+    var prevImg = currentImg.prev();
+    if (prevImg.length) {
+      currentImg.removeClass("active").css("z-index", -10);
+      prevImg.addClass("active").css("z-index", 10);
+    }
   });
 });
