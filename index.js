7827d9ae88a4a3a7eddfb7e499991aeef3a2cff6
Henry Liu <liuhenry64@gmail.com>
2022-03-13 23:17:33 -0400

    Add fade ins on pictures

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ $(document).ready(function () {
     var currentImg = $(".active");
     var nextImg = currentImg.next();
     if (nextImg.length) {
-      currentImg.removeClass("active").css("z-index", -10);
-      nextImg.addClass("active").css("z-index", 10);
+      currentImg.fadeOut(400, () => {
+        currentImg.removeClass("active").css("z-index", -10);
+        nextImg.fadeIn(400);
+        nextImg.addClass("active").css("z-index", 10);
+      });
     }
   });
   $("#prev-arrow").on("click", () => {
     var currentImg = $(".active");
     var prevImg = currentImg.prev();
     if (prevImg.length) {
-      currentImg.removeClass("active").css("z-index", -10);
-      prevImg.addClass("active").css("z-index", 10);
+      currentImg.fadeOut(400, () => {
+        currentImg.removeClass("active").css("z-index", -10);
+        prevImg.addClass("active").css("z-index", 10);
+        prevImg.fadeIn(400);
+      });
     }
   });
 });
